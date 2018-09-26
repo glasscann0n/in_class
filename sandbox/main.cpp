17b8e746d571185030e6719f07bdd1f2de11661a
Adrian Rasmussen <dane@jigsolve.com>
2018-09-26 09:35:27 -0500

    inclass stuff

---
+++ sandbox/main.cpp
@@ -0,0 +1,7 @@
+#include "Message.h"
+
+
+int main(int argc, char * argv[]) {
+
+   message();
+}
---
+++ sandbox/message.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+#include "message.h"
+
+void message(void) {
+    cout << "Hello, World" << endl;
+}
