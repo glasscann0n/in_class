6c93a2ad2997c9b44333fb072fbd9b24f6aa8525
Adrian Rasmussen <dane@jigsolve.com>
2018-10-29 09:40:02 -0500

    adding

---
+++ modular-make/README.rst
@@ -0,0 +1,18 @@
+Modular Make
+############
+:Author: Roie R. Black
+:Date: Octpber 27, 2018
+:License: BSD 3-Clause
+:Email: rblack@austincc.edu
+
+A Makefile system designed to be used by students learning Python and C/C++
+programming. This tool is being used by my students at Austin Community College
+on all major platforms.
+
+Documentation
+*************
+
+DOcumentation for this project is produced using the Python Sphinx tools. Of course, the ``Makefile`` does alll the hard work. The resulting html files are visible on Github at the following URL:
+
+    * http://rblack42.github.io/modular-make
+
---
+++ modular-make/rst/conf.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import cloud_sptheme as csp
+
+sys.path.insert(0, os.path.abspath('.'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'ModularMake'
+copyright = '2018, Roie R. Black'
+author = 'Roie R. Black'
+
+version = '0.0.1'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.githubpages',
+]
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+language = None
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+pygments_style = None
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'cloud'
+html_theme_path = [csp.get_theme_dir()]
+html_theme_options = { "roottarget": "index" }
+html_static_path = ['_static']
+
+# -- Options for LaTeX output ------------------------------------------------
+latex_elements = {
+    'papersize': 'letterpaper',
+    'pointsize': '12pt',
+    'figure_align': 'htbp',
+}
+
+latex_documents = [
+    (master_doc, 'ModularMake.tex', 'Modular Make',
+     'Roie R. Black', 'manual'),
+]
+
+# -- Extension configuration -------------------------------------------------
